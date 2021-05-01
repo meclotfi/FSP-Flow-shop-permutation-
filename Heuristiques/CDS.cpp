d7c23b036828b59498b64e58a972dabe96950b36
unknown <hd_idrissou@esi.dz>
2021-05-01 17:05:02 +0100

    CDS

--- Heuristiques/CDS.cpp
+++ Heuristiques/CDS.cpp
@@ -43,40 +43,93 @@ void johnson(int A[500][20], int nbJobs, int &cmax, vector<int> &solution, int d
     }
     cmax=Cmax(solution, data, 2);
 }
+/////////////////////////////////////Main pour essai de  johnson ///////////////////////////////////////
+// int main(int argc, char const *argv[])
+// {
+//     int A[500][20];
+//     A[0][0]=20;
+//     A[1][0]=10;
+//     A[2][0]=15;
+//     A[3][0]=32;
+//     A[4][0]=3;
+//     A[0][1]=55;
+//     A[1][1]=23;
+//     A[2][1]=5;
+//     A[3][1]=4;
+//     A[4][1]=45;
+//     int cmax;
+//     vector<int> solution ;
+//     /*
+//     vector< pair<int, int> > my_u;
+//     my_u=U(A,5);
+//     for(int i = 0; i < my_u.size(); i++)
+//     {
+//         cout << my_u[i].first << ", " << my_u[i].second << endl;
+//     }
+//     vector< pair<int, int> > my_u2;
+//     my_u2=V(A,5);
+//     for(int i = 0; i < my_u2.size(); i++)
+//     {
+//         cout << my_u2[i].first << ", " << my_u2[i].second << endl;
+//     }
+//     */
+//    johnson(A, 5, cmax, solution, A);
+//    for(int i = 0; i < solution.size(); i++)
+//     {
+//         cout << solution[i] << endl;
+//     }
+//     cout << cmax <<endl;
+//     return 0;
+// }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//recuperer structure de donnees
+//for m-1 sequence construire et appeler johnson qui retourne une sequence appeler cmax levaluer stocker result tableau de m-1 case et prendre le min 
+
 int main(int argc, char const *argv[])
 {
-    int A[500][20];
-    A[0][0]=20;
-    A[1][0]=10;
-    A[2][0]=15;
-    A[3][0]=32;
-    A[4][0]=3;
-    A[0][1]=55;
-    A[1][1]=23;
-    A[2][1]=5;
-    A[3][1]=4;
-    A[4][1]=45;
-    int cmax;
-    vector<int> solution ;
-    /*
-    vector< pair<int, int> > my_u;
-    my_u=U(A,5);
-    for(int i = 0; i < my_u.size(); i++)
+    int nbJobs, nbMachines, A[500][20], Seq[500][20], somme1 = 0, somme2 =0, Cmax, c = INT32_MAX; 
+    vector<int> solution, solutionsauv;
+
+    loader("../benchmarks/20jobs10machines.txt", &nbJobs, &nbMachines, A); 
+ 
+    for (int k = 0; k < (nbMachines - 1 ); k++)
     {
-        cout << my_u[i].first << ", " << my_u[i].second << endl;
+        //construire tableau de nbJobs 2 machines 
+        for (int i = 0; i < nbJobs; i++)
+        {
+            for (int j = 0; j <= k; j++) {somme1 = somme1 + A[i][j];}
+            for (int j = nbMachines-1; j >= nbMachines-1-k; j--) {somme2 = somme2 + A[i][j];}
+            Seq[i][0] = somme1;
+            Seq[i][1] = somme2;
+            somme1 = 0;   somme2 = 0;
+        }
+
+        //lui appliquer johnson 
+        Cmax = 0;
+        solution.clear();
+
+       
+        cout <<"cmax"<< Cmax <<endl;
+        cout << nbJobs <<endl;
+
+        johnson(Seq, nbJobs, Cmax, solution, A);
+        for(int i = 0; i < nbJobs; i++)
+        {
+            cout << solution[i] << endl;
+        }
+        if ( Cmax < c )
+        {
+            c = Cmax;
+            solutionsauv = solution;
+        }
+    
+
     }
-    vector< pair<int, int> > my_u2;
-    my_u2=V(A,5);
-    for(int i = 0; i < my_u2.size(); i++)
-    {
-        cout << my_u2[i].first << ", " << my_u2[i].second << endl;
-    }
-    */
-   johnson(A, 5, cmax, solution, A);
-   for(int i = 0; i < solution.size(); i++)
-    {
-        cout << solution[i] << endl;
-    }
-    cout << cmax <<endl;
-    return 0;
+    //On aura obtenu le c qui est le cmax et solutionsauv qui contient la sequence
+    for(int i = 0; i < nbJobs; i++)
+       {
+           cout << solutionsauv[i] << endl;
+       }
+       cout << c <<endl;
 }
