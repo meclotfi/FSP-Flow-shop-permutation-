deae08cfc120376600d5ee1b3d463bf35b25f3c1
kadem khadidja <hk_kadem@esi.dz>
2021-05-01 12:16:56 +0100

    adding evaluation

--- exact/exact.cpp
+++ exact/exact.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 int nbJobs, nbMachines, somme, A[500][20];
-int M=INT32_MAX;
+int M = INT32_MAX;
 
 // comparaison function to sort vectors
 bool cmp(pair<int, int> &a,
@@ -18,83 +18,86 @@ bool cmp(pair<int, int> &a,
 {
     return a.second > b.second;
 }
-int DFS(list<int> S,list<int> J)
+int DFS(list<int> S, list<int> J)
 {
 
-if (!S.empty()) cout<<"J"<<S.back()<<" ";
-if (J.empty()) {cout << "\n"; return 0;}
-
-list<int> J1,S1;
-//
-for (auto &it : J)
+    if (!S.empty())
+        cout << "J" << S.back() << " ";
+    if (J.empty())
     {
-        S1=S;
+        cout << "\n";
+        return 0;
+    }
+
+    list<int> J1, S1;
+    //
+    for (auto &it : J)
+    {
+        S1 = S;
         S1.push_back(it);
-        J1=J;
+        J1 = J;
         J1.remove(it);
-    // calculer le cout
-    vector<int> sol(S1.begin(), S1.end());
-    int cost=Cmax(sol,A,nbMachines);
+        // calculer le cout
+        vector<int> sol(S1.begin(), S1.end());
+        int cost = Cmax(sol, A, nbMachines);
 
-    // tester si feuille et caluler l'evaluation sinon
-    if (J1.empty())
-    {
-        cout<<"J"<<S1.back()<<" ";
+        // tester si feuille et caluler l'evaluation sinon
+        if (J1.empty())
+        {
+            cout << "J" << S1.back() << " ";
 
-        //update lower bound 
-        if(M>cost)
-        { 
-          M=cost;
-          cout<<"update M= "<<M<<" ";
+            //update lower bound
+            if (M > cost)
+            {
+                M = cost;
+                cout << "update M= " << M << " ";
+            }
+        }
+        else
+        {
+            //calculer l'evaluation:
+            int evaluation = eval(cost, A, nbMachines, J);
+
+            //tester si possible d'elager
+            if (M < evaluation)
+                // elagage
+
+                // brunshing
+                DFS(S1, J1);
         }
 
-    }
-    else
-    {
-        //calculer l'evaluation:
-
-        //tester si possible d'elager 
-
-        // brunshing
-        DFS(S1,J1);
-    }
-    
-
-    
-        cout<<"back \n";
+        cout << "back \n";
     }
 }
 int main()
 {
     // solution : the solution vector
-    
-    list<int> J,S;
+
+    list<int> J, S;
     vector<int> solution;
 
     //load nbJobs, nbMachines and the matrix A
-    string filepath="../benchmarks/VFR10_5_1_Gap.txt";
-    loader(filepath,&nbJobs, &nbMachines, A);
+    string filepath = "../benchmarks/VFR10_5_1_Gap.txt";
+    loader(filepath, &nbJobs, &nbMachines, A);
 
+    //initiaalization des structure de données
+    //J: l'enssemble des job non encore assigner
+    vector<pair<int, int>> sommeLigne;
+    pair<int, int> c;
 
-//initiaalization des structure de données
-//J: l'enssemble des job non encore assigner
-vector<pair<int,int>> sommeLigne;
-pair<int,int> c;
-
-for (int i = 0; i < nbJobs; i++)
+    for (int i = 0; i < nbJobs; i++)
     {
-        c.first=i;
+        c.first = i;
         c.second = accumulate(A[i], A[i] + nbMachines, somme);
         sommeLigne.push_back(c);
-
     }
-sort(sommeLigne.begin(), sommeLigne.end(), cmp);
+    sort(sommeLigne.begin(), sommeLigne.end(), cmp);
 
-for (int j = 0; j < nbMachines; j++)
-        {
-             J.push_back(sommeLigne[j].first);
-         }
+    for (int j = 0; j < nbMachines; j++)
+    {
+        J.push_back(sommeLigne[j].first);
+    }
 
-DFS(S,J);
-return 0;  
+    DFS(S, J);
+    return 0;
 }
