f3fcaee952bf38b745348df502c2e3b23235ac53
Meclotfi <hl_mecharbat@esi.dz>
2021-05-05 01:32:04 +0100

    parallel BB

--- exact/exact.cpp
+++ exact/exact.cpp
@@ -7,6 +7,8 @@
 #include <numeric>
 #include "../FSP.h"
 #include "string.h"
+#include "omp.h"
+#define CHUNKSIZE 1
 
 using namespace std;
 
@@ -22,23 +24,32 @@ int DFS(list<int> S, list<int> J,int &M,int nbMachines,int A[500][20])
 {
 
    if (!S.empty())
-        cout << "J" << S.back() << " ";
+        //cout << "J" << S.back() << " ";
     if (J.empty())
     {
-        cout << "\n";
+        //cout << "\n";
         return 0;
     }
     
-
-    list<int> J1, S1;
+list<int> J1, S1;
+    
     
     for (auto &it : J)
     {
+        
         S1 = S;
         S1.push_back(it);
         J1 = J;
         J1.remove(it);
 
+    /*
+        for (auto &it : S1)
+            {
+              cout << "J#" << it<<" ";
+               }
+                        cout<<"\n";
+                       
+  */
         // calculer le cout
         vector<int> sol(S1.begin(), S1.end());
         int cost = Cmax(sol, A, nbMachines);
@@ -46,14 +57,21 @@ int DFS(list<int> S, list<int> J,int &M,int nbMachines,int A[500][20])
         // tester si feuille et caluler l'evaluation sinon
         if (J1.empty())
         {
-            cout << "J" << S1.back() << "\n";
+            /*
+            
+            for (auto &it : S1)
+            {
+              cout << "J" << it<<" ";
+            }
+            cout << " cost= " << cost<<"\n";
+            */
             // cout<<"a ";
             //update lower bound
             if (M > cost)
             {
-                cout << "M= " << M << " ";
+                //cout << "M= " << M << " ";
                 M = cost;
-                cout << "update M= " << M << " ";
+                //cout << "update M= " << M << " ";
             }
         }
         else
@@ -90,7 +108,7 @@ vector<int> solution;
 
     for (int i = 0; i < nbJobs; i++)
     {
-        c.first = i;
+        c.first = i+1;
         c.second = accumulate(A[i], A[i] + nbMachines, somme);
         sommeLigne.push_back(c);
         cout<<"sum_ligne "<<c.second<<" \n";
@@ -103,7 +121,57 @@ vector<int> solution;
         
     }
    
+   
     DFS(S,J,M,nbMachines,A);
+    cout<< "final M= "<<M;
+    return M;
+}
+int Parallel_BB(int nbJobs,int nbMachines,int A[500][20]) 
+{
+
+int M=INT32_MAX;
+int somme;
+vector<int> J, S;
+vector<int> solution;
+
+//initiaalization des structure de données
+    //J: l'enssemble des job non encore assigner
+    vector<pair<int, int>> sommeLigne;
+    pair<int, int> c;
+
+    for (int i = 0; i < nbJobs; i++)
+    {
+        c.first = i+1;
+        c.second = accumulate(A[i], A[i] + nbMachines, somme);
+        sommeLigne.push_back(c);
+        cout<<"sum_ligne "<<c.second<<" \n";
+    }
+    sort(sommeLigne.begin(), sommeLigne.end(), cmp);
+
+    for (int j = 0; j < nbJobs; j++)
+    {
+        J.push_back(sommeLigne[j].first);
+        
+    }
+ list<int> S1; 
+ list<int> J1(J.begin(), J.end());
+ std::list<int>::iterator it=J1.begin();
+ 
+ #pragma omp parallel for  schedule(dynamic,CHUNKSIZE) firstprivate(J1,S1)
+   for (int i = 0; i < nbJobs; i++)
+    {
+        printf("Thread %d starting  ...\n", omp_get_thread_num());
+       
+     
+       S1.push_back(J[i]);
+             
+        J1.remove(J[i]);
+        it++;
+       
+        DFS(S1 , J1 , M , nbMachines , A );
+
+    }
+   
     cout<< "final M= "<<M;
     return M;
 }
@@ -116,9 +184,13 @@ int main()
     
   
     //load nbJobs, nbMachines and the matrix A
-    string filepath = "../benchmarks/20jobs10machines.txt";
+    string filepath = "../benchmarks/VFR10_5_7_Gap.txt";
     loader(filepath, &nbJobs, &nbMachines, A);
-    BB(nbJobs,nbMachines,A);
+    
+    //vector<int> j={3,17,15,8,9,6,5,14,16,7,11,13,18,19,1,4,2,10,20,12};
+    //int ev=Cmaxt(j,A,nbMachines);
+    //cout<<ev;
+    Parallel_BB(nbJobs,nbMachines,A);
    
     
     return 0;
