0fb9f0844af3854e73ed70391837363816f5806b
LiNaNiNa <hl_mezdour@esi.dz>
2021-05-05 18:18:25 +0100

    Add: New Cmax function that returns vectors ( to minimize space and execution time )

--- FSP.h
+++ FSP.h
@@ -101,3 +101,49 @@ int eval(int CActuel, int A[500][20], int nbMachines, list<int> J)
         C += A[it][nbMachines - 1];
     return C;
 }
+
+// if newJob = -1 : 'L' contains a list of jobs whose Cmax is to be computed
+// else, 'L' contains the exection times of the last job on all the machines ( + '0' in the first position) 
+// OUTPUT: a vector of the newJob execution, with same structure as L
+// NOTE: Please access to the Cmax using: res[nbMachines].
+vector<int> Cmax_Add_Job(vector<int> L, int newJob, int A[500][20], int nbMachines)
+{
+    if( nbMachines == 0)
+        return vector<int>(); // ERROR!
+    
+    vector<int> res(nbMachines+1);
+    if( newJob == -1) // normal execution of Cmax but returning the last line of the matrix.
+    {
+        int nbJobs = L.size();
+        int matrix[nbJobs + 1][nbMachines + 1];
+        for (int j = 0; j <= nbMachines; j++)
+            matrix[0][j] = 0;
+
+        for (int i = 0; i <= nbJobs; i++)
+            matrix[i][0] = 0;
+
+        for (int i = 1; i <= nbJobs; i++)
+        {
+            for (int j = 1; j <= nbMachines; j++)
+            {
+                matrix[i][j] = A[L[i - 1]][j - 1]; // because btw 0-19
+                matrix[i][j] += (matrix[i - 1][j] < matrix[i][j - 1]) ? matrix[i][j - 1] : matrix[i - 1][j];
+            }
+        }
+
+        for (int j=0; j <= nbMachines; j++) {
+            res[j] = matrix[nbJobs][j];
+        }
+        return res; 
+    }
+    else
+    {
+        res[0] = 0;
+        for (int j = 1; j <= nbMachines; j++)
+        {
+            res[j] = A[newJob][j - 1]; // we suppose jobs start from 0
+            res[j] += (L[j] < res[j - 1]) ? res[j - 1] : L[j];
+        }
+        return res;
+    }
+}
